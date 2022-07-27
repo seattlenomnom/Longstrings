0e78267b2018f79fc6b960ff8473a3dd81bb0cff
seattlenomnom <mark.crapser@gmail.com>
2022-07-26 18:02:51 -0700

    added material to README and text of longstrings.c

--- longstrings.c
+++ longstrings.c
@@ -3,5 +3,10 @@
  * strings in a program. It will also experiment with using universal char-
  * acters.
  *
+ * A program written by me to experiment with creating long strings and using
+ * universal characters.
+ *
+ * Programmer: Mark Crapser
+ *
  *
  */
